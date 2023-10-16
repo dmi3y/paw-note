32b74b7dfd1bf59c447f53f7e29ba3decdb32376
dmi3y <dmitrii@matternet.us>
2023-10-16 13:26:28 -0700

    chore: debuggin

--- src/components/Subscription.tsx
+++ src/components/Subscription.tsx
@@ -20,6 +20,9 @@ export default function Subscription({
       });
     if (subscription) {
       await postToServer("/api/add-subscription", subscription);
+    } else {
+      console.log("subscription is null");
+      console.log({ serviceWorker });
     }
     checkServiceWorker();
   }
